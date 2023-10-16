b62ffa768352edf0394ed92eb495ecb68837af6e
ateeqbhatti <111612103+ateeqbhatti@users.noreply.github.com>
2023-10-16 20:35:46 +0500

    Initializing

---
+++ src/components/NavBar.js
@@ -0,0 +1,292 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+import {
+  TwitterIcon,
+  FacebookIcon,
+  GithubIcon,
+  LinkedInIcon,
+  InstagramIcon,
+  SunIcon,
+  MoonIcon,
+} from "./Icons";
+import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
+import { useState } from "react";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span
+        className={`h-[1px] inline-block  bg-dark absolute left-0 -bottom-0.5 
+        group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }
+        dark:bg-light`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      className={`${className} relative group text-light dark:text-dark my-2`}
+      onClick={handleClick}
+    >
+      {title}
+
+      <span
+        className={`h-[1px] inline-block  bg-light absolute left-0 -bottom-0.5 
+        group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }
+        dark:bg-dark`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
+const NavBar = () => {
+  const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
+      <button
+        className="flex-col justify-center items-center hidden lg:flex pt-3 absolute left-10 right-auto top-8"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          }`}
+        ></span>
+      </button>
+
+      <div className="w-full flex justify-between items-center lg:hidden">
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-4 " />
+          <CustomLink href="/about" title="About" className="mx-4 " />
+          <CustomLink href="/projects" title="Projects" className="mx-4 " />
+          {/* <CustomLink
+              href="/articles"
+              title="Articles"
+              className="ml-4 "
+              
+            /> */}
+        </nav>
+
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://twitter.com/Ateeeeq2"     
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-7 mr-3 dark:fill-light"
+          >
+            <TwitterIcon />
+          </motion.a>
+          {/* Twitter */}
+
+          <motion.a
+            href="https://github.com/ateeqbhatti"      
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-7 mx-3"
+          >
+            <GithubIcon />
+          </motion.a>
+          {/* GitHub */}
+
+          <motion.a
+            href="https://www.linkedin.com/in/ateeq-bhatti-b04aa3152?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"    
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <LinkedInIcon />
+          </motion.a>
+          {/* Linked In */}
+
+          <motion.a
+            href="https://instagram.com/ateeeeeeeeeeeq?igshid=MzNlNGNkZWQ4Mg=="   
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-8 mx-3 " 
+          >
+            <InstagramIcon />
+          </motion.a>
+          {/* Instagram */}
+
+          <motion.a
+            href="https://www.facebook.com/ateeq.bhatti.71?mibextid=2JQ9oc"        
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-8 ml-3 mr-3"
+          >
+            <FacebookIcon />
+          </motion.a>
+          {/* Facebook */}
+
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1 
+            ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+            `}
+          >
+            {mode === "dark" ? (
+              <SunIcon className={"fill-dark"} />
+            ) : (
+              <MoonIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
+
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
+        >
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href="/"
+              title="Home"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="Projects"
+              className=""
+              toggle={handleClick}
+            />
+          </nav>
+          <nav className="flex items-center justify-center flex-wrap">
+            <motion.a
+              href="https://twitter.com/Ateeeeq2"        
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              
+              className="w-7 mr-3 sm:fill-light sm:dark:fill-dark sm:mr-0"
+            >
+              
+              <TwitterIcon />
+            </motion.a>
+            {/* Twitter */}
+
+            <motion.a
+              href="https://github.com/ateeqbhatti"         
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 bg-light rounded-full dark:bg-dark  sm:mr-1"
+            >
+              <GithubIcon />
+            </motion.a>
+            {/* GitHub */}
+
+            <motion.a
+              href="https://www.linkedin.com/in/ateeq-bhatti-b04aa3152?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"   
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:-mr-1 dark:sm:mr-1" 
+            >
+              <LinkedInIcon />
+            </motion.a>
+            {/* Linked In */}
+
+            <motion.a
+              href="https://instagram.com/ateeeeeeeeeeeq?igshid=MzNlNGNkZWQ4Mg=="   
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-[25px] mx-3 sm:w-[40px] sm:dark:w-[25px] dark:bg-dark dark:rounded-lg sm:-mr-2 dark:sm:mr-0" 
+            >
+              <InstagramIcon />
+            </motion.a>
+            {/* Instagram */}
+
+            <motion.a
+            href="https://www.facebook.com/ateeq.bhatti.71?mibextid=2JQ9oc"   
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-8 mx-3 sm:mr-1"
+          >
+            <FacebookIcon />
+          </motion.a>
+          {/* Facebook */}
+          
+
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-3 flex items-center justify-center rounded-full p-1 
+            ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+            `}
+            >
+              {mode === "dark" ? (
+                <SunIcon className={"fill-dark"} />
+              ) : (
+                <MoonIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>{" "}
+        </motion.div>
+      ) : null}
+
+      <div className="absolute left-[50%] top-5 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
+
+
---
+++ src/pages/about.js
@@ -0,0 +1,125 @@
+import Animatedtext from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import profilePic from "../../public/images/profile/Portfolio-About.webp";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import HireMe from "@/styles/HireMe";
+import TransitionEffect from "@/components/TransitionEffect";
+
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 })
+  const isInView = useInView(ref, {once: true});
+
+  useEffect(() => {
+    if (isInView){
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+        if(ref.current && latest.toFixed(0) <= value){
+            ref.current.textContent = latest.toFixed(0);
+        }
+    })
+
+  },[springValue, value])
+
+
+  return <span ref={ref}></span>;
+};
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Ateeq Ahmad | About</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <TransitionEffect />
+      <main className="flex w-full flex-col items-center justify-center dark:text-light md:pt-10 sm:pt-10 xs:pt-10">
+        <Layout className="pt-16">
+          <Animatedtext 
+          text="Passion Fuels Purpose!" 
+          className="mb-16 dark:text-light lg:!text-7x md:!text-5xl sm:!text-4xl xs:!text-4xl sm:mb-8" />
+
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8 ">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:!col-span-8 ">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                Biography
+              </h2>
+
+              <p className="font-medium xl:text-justify">
+              Hello, I am Ateeq Ahmad, a Python Django full-stack developer with a strong commitment to crafting functional and user-centric web applications. With several years of expertise in the field, I continually seek creative and innovative approaches to translate visions of my clients into reality.
+              </p>
+
+              <p className=" my-4 font-medium xl:text-justify">
+              I firmly believe that development goes beyond mere functionality – it's about problem-solving and crafting seamless, user-friendly experiences.
+              </p>
+
+              <p className="font-medium xl:text-justify">
+              Be it a web application, mobile app, or any digital project, I bring my unwavering dedication to technical excellence and user-centered design philosophy to each undertaking. I eagerly anticipate the chance to apply my skills and enthusiasm to your upcoming project as a Python Django full-stack developer.
+              </p>
+            </div>
+
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:!col-span-8 sm:!col-span-8 xs:!col-span-8  md:mx-auto">
+
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+              <Image
+                src={profilePic}
+                alt="Awais Ashraf"
+                className="w-full h-auto rounded-2xl "
+                priority
+                sizes="(max-width:768px) 100vw,
+                (max-width:1200px) 50vw,
+                33vw"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:!items-center ">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                    <AnimatedNumbers value={10} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  Satisfied clients
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center xl:!items-center ">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                <AnimatedNumbers value={20} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  Projects Completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center xl:!items-center ">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                <AnimatedNumbers value={12} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  Months of Experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        <Skills />
+        <Experience />
+        <Education />
+        </Layout>
+        <HireMe />
+      </main>
+    </>
+  );
+};
+
+export default about;
